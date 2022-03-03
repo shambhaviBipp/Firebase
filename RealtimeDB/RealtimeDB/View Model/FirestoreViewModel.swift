206af2ec7a71b1859d8250ca3cb0c1476f2a560c
shambhaviBipp <shambhavid@bipp.io>
2022-03-03 10:51:48 +0530

    Added ID for firestore

--- RealtimeDB/RealtimeDB/View Model/FirestoreViewModel.swift
+++ RealtimeDB/RealtimeDB/View Model/FirestoreViewModel.swift
@@ -12,14 +12,16 @@ class FirestoreViewModel {
     
     var dataModel = [UsersData]()
     let db = Firestore.firestore()
+    var count = Int()
     
     func addFirestoreData(name: String, mail: String, address: String, completion: @escaping (_ result: String?) -> Void ){
         
         var ref: DocumentReference? = nil
-        ref = db.collection("Data").addDocument(data: [
+        ref = db.collection("Data1").addDocument(data: [
             "Name": name,
             "Mail": mail,
-            "Address": address
+            "Address": address,
+            "Id": count + 1
         ]) { err in
             if err != nil {
                 completion("error")
@@ -35,24 +37,26 @@ class FirestoreViewModel {
     
     func getFireStoreData(record: String, completion: @escaping (_ result: [UsersData]?) -> Void){
         if record == "all"{
-            db.collection("Data").order(by: "Name").getDocuments() { (querySnapshot, err) in
+            db.collection("Data1").order(by: "Id").getDocuments() { (querySnapshot, err) in
                 if err == nil {
                     for document in querySnapshot!.documents {
                         let doc = document.data()
                         self.dataModel.append(UsersData(name: doc["Name"] as? String ?? "-", mail: doc["Mail"] as? String ?? "-", address: doc["Address"] as? String ?? "-"))
                     }
+                    self.count = self.dataModel.count
                     completion(self.dataModel)
                 } else {
                     completion(nil)
                 }
             }
         }else if record == "latest"{
-            db.collection("Data").order(by: "Name").limit(toLast: 1).getDocuments() { (querySnapshot, err) in
+            db.collection("Data1").order(by: "Id").limit(toLast: 1).getDocuments() { (querySnapshot, err) in
                 if err == nil {
                     for document in querySnapshot!.documents {
                         let doc = document.data()
                         self.dataModel.append(UsersData(name: doc["Name"] as? String ?? "-", mail: doc["Mail"] as? String ?? "-", address: doc["Address"] as? String ?? "-"))
                     }
+                    self.count = self.dataModel.count
                     completion(self.dataModel)
                 } else {
                     completion(nil)
